76b3e7bb1eb067d4f108f16fa4737f3471e1ac5d
Dinesh Matta <dinesh.matta@synechron.com>
2017-09-24 01:43:24 +0530

    changing server.js to mock GET api response as POST

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
-var jsonServer = require('json-server');
-var server = jsonServer.create();
-var router = jsonServer.router('db.json');
-var middlewares = jsonServer.defaults();
-var fs = require('fs');
-var port = process.env.PORT || 3000;
+// server.js
+var jsonServer = require('json-server')
+var server = jsonServer.create()
+var router = jsonServer.router('db.json')
+var middlewares = jsonServer.defaults()
 
-server.use(middlewares);
-server.use(router);
+server.use(middlewares)
 
-server.listen(port, function () {
-  console.log('\x1b[36mjson-server is running!');
-});
+server.use(jsonServer.bodyParser)
+server.use(function (req, res, next) {
+  if (req.method === 'POST') {
+    // Converts POST to GET and move payload to query params
+    // This way it will make JSON Server that it's GET request
+    req.method = 'GET'
+    req.query = req.body
+  }
+  // Continue to JSON Server router
+  next()
+})
+
+// If you need to scope this behaviour to a particular route, use this
+server.post('/comments', function (req, res, next) {
+  req.method = 'GET'
+  req.query = req.body
+  next()
+})
+
+server.use(router)
+server.listen(3000, function () {
+  console.log('JSON Server is running')
+})
